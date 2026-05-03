28dc853b9e502d29d0158c8339705b44545a86b5
KOKUMUbooker <bookerochieng20@gmail.com>
2026-05-03 21:01:28 +0300

    Project initial setup

---
+++ VectorFlow.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace VectorFlow.Shared;
+
+public class Class1
+{
+
+}
---
+++ VectorFlow.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace VectorFlow.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
