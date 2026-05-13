3e04cb0baaefb21b85e1d8804dbac0c7bdda63e0
KOKUMUbooker <bookerochieng20@gmail.com>
2026-05-13 17:27:15 +0300

    Define ProjectService structure in an interface

---
+++ VectorFlow.Client/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿namespace VectorFlow.Client.Services.Interfaces;
+
+using VectorFlow.Shared.DTOs;
+
+public interface IProjectService
+{
+    /// <summary>Returns all projects of a workspace.</summary>
+    Task<ServiceResult<List<ProjectDto>>> GetWorkspaceProjects(Guid workspaceId);
+
+    /// <summary> Creates a project then returns the created project </summary>
+    Task<ServiceResult<ProjectDto>> CreateProjectAsync(Guid workspaceId, CreateProjectRequest request);
+
+    /// <summary>Returns a single project.</summary>
+    Task<ServiceResult<ProjectDto>> GetProjectAsync(Guid workspaceId, Guid projectId);
+
+    Task<ServiceResult> DeleteProjectAsync(Guid workspaceId, Guid projectId);
+    Task<ServiceResult<ProjectDto>> UpdateProjectAsync(Guid projectId, UpdateProjectRequest request);
+}
